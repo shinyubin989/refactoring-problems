cdf361a4ebb77cc3ba693e6b8479d8f8b9f26da6
shinyubin989 <dbqls2821@gmail.com>
2024-01-25 22:42:20 +0900

    fake repository 클래스 생성

---
+++ mail-server/src/test/kotlin/com/gitub/oopgurus/refactoringproblems/mailserver/FakeMailRepository.kt
@@ -0,0 +1,43 @@
+package com.gitub.oopgurus.refactoringproblems.mailserver
+
+import org.springframework.data.domain.Pageable
+import java.util.concurrent.atomic.AtomicLong
+
+class FakeMailRepository : MailRepository {
+
+    private val id = AtomicLong(1)
+    private val mails = mutableMapOf<Long, MailEntity>()
+
+    override fun findByToAddressOrderByCreatedAt(recipient: String, pageable: Pageable): List<MailEntity> {
+        return mails.values.filter { it.toAddress == recipient }.sortedBy { it.createdAt }
+                .subList(pageable.offset.toInt(), pageable.offset.toInt() + pageable.pageSize)
+    }
+
+    override fun save(mailEntity: MailEntity): MailEntity {
+        var mailId = mailEntity.id
+        if (mailId == null) {
+            mailId = id.getAndIncrement()
+            val newMail = MailEntity(
+                    mailId,
+                    mailEntity.title,
+                    mailEntity.fromAddress,
+                    mailEntity.fromName,
+                    mailEntity.toAddress,
+                    mailEntity.htmlTemplateName,
+                    mailEntity.htmlTemplateParameters,
+                    mailEntity.isSuccess,
+                    mailEntity.createdAt,
+                    mailEntity.updatedAt,
+
+            )
+            mails[mailId] = newMail
+            return newMail
+        }
+        mails[mailId] = mailEntity
+        return mailEntity
+    }
+
+    override fun findAll(): List<MailEntity> {
+        return mails.values.toList()
+    }
+}
